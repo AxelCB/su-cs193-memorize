3a099e98475aef9312fdc423fe2693baebd9d375
Axel Collard Bovy (MacbookHx) <axcbovy@gmail.com>
2020-08-18 20:45:44 -0300

    Improving game's choosing card logic to actually be playable, matching cards and updating them acordingly. Adding another extension to array to cleanup some code

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -2,6 +2,16 @@ import Foundation
 
 struct MemoryGame<CardContent: Equatable> {
     private(set) var cards: Array<Card>
+    private var indexOfTheOnlyAndOnlyFaceUpCard: Int? {
+        get {
+            return cards.indices.filter { cards[$0].isFaceUp && !cards[$0].isMatched }.only
+        }
+        set {
+            for cardIndex in cards.indices {
+                cards[cardIndex].isFaceUp = cardIndex == newValue
+            }
+        }
+    }
     
     init(pairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = []
@@ -15,7 +25,15 @@ struct MemoryGame<CardContent: Equatable> {
     
     mutating func choose(card chosenCard: Card) {
         if let chosenCardIndex = cards.firstIndex(matching: chosenCard), !cards[chosenCardIndex].isFaceUp, !cards[chosenCardIndex].isMatched {
-            cards[chosenCardIndex].isFaceUp = true
+            if let faceUpNotMatchedCardIndex = indexOfTheOnlyAndOnlyFaceUpCard {
+                if cards[faceUpNotMatchedCardIndex].content == cards[chosenCardIndex].content {
+                    cards[chosenCardIndex].isMatched = true
+                    cards[faceUpNotMatchedCardIndex].isMatched = true
+                }
+                cards[chosenCardIndex].isFaceUp = true
+            } else {
+                indexOfTheOnlyAndOnlyFaceUpCard = chosenCardIndex
+            }
         }
     }
     
